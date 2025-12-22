fa9865ac3836231a34b6496a3ab0a00f71da15a0
Christian Hesse <mail@eworm.de>
2025-12-22 21:32:00 +0100

    sort includes alphabetically

--- udp514-journal.h
+++ udp514-journal.h
@@ -21,17 +21,17 @@
 
 #define _POSIX_C_SOURCE 200809L
 
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <netdb.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
 #include <errno.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 #include <time.h>
+#include <unistd.h>
 
 #define SYSLOG_NAMES
 #define __USE_MISC
